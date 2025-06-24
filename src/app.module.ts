155e22fc40b6cfa01178e3d0541cef7a87a19ca3
Jonatas Marques <jonatasmarquessousa@gmail.com>
2025-06-24 13:57:21 -0300

    feat: estrutura do place

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PlaceModule } from './place/place.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PlaceModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
