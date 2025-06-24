89da4b5af419f867a7b9538c0d4bab0026a4463b
Lianderson <liandersonr18@gmail.com>
2025-06-24 16:26:57 -0300

    arruma as rotas get

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PlaceService } from './place.service';
+import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('place')
 export class PlaceController {
 
     constructor(private placeService: PlaceService){}
 
+    @Get('all')
+    findAll(){
+        return this.placeService.findAll()
+    }
+
     @Get()
-    findAll(@Query('type') type: string){
+    findAllfromTyoe(@Query('type') type?: string){
         if(type){
             return this.placeService.findAllFromType(type)
         }
-        return this.placeService.findAll()
     }
 }
