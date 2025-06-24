950c97784c1977b14077f1f4ee48095268aa8ae6
Lianderson <liandersonr18@gmail.com>
2025-06-24 16:31:59 -0300

    feat: documenta as rotas get

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -8,11 +8,16 @@ export class PlaceController {
     constructor(private placeService: PlaceService){}
 
     @Get('all')
+    @ApiOperation({summary: 'Listar Todos os locais'})
+    @ApiResponse({status:200, description: "Lista de locais retornada com sucesso!!"})
     findAll(){
         return this.placeService.findAll()
     }
 
     @Get()
+    @ApiOperation({summary: 'Listar Todos os locais por tipo'})
+    @ApiQuery({name:'type',type:String, description:'Tipo '})
+    @ApiResponse({status:200,description: 'Listar locais pelo o tipo retornada com sucesso!!'})
     findAllfromTyoe(@Query('type') type?: string){
         if(type){
             return this.placeService.findAllFromType(type)
