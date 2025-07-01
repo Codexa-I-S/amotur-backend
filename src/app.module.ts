daca6ef1660d3e233797b7d3f2491ea43239fc42
Jonatas Marques <jonatasmarquessousa@gmail.com>
2025-07-01 16:57:13 -0300

    feat: register dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlaceModule } from './place/place.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PlaceService } from './place/place.service';
-import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PlaceModule, PrismaModule, UserModule, AuthModule],
+  imports: [PlaceModule, PrismaModule, AuthModule],
   controllers: [],
   providers: []
 })
