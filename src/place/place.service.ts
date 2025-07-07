0a26ea4f4fcb1613aba9d0c8c8ca09b84863c4f2
Lianderson <liandersonr18@gmail.com>
2025-07-07 13:56:26 -0300

    feat:uma verificação de coordenadas antes de adicionar

--- src/place/place.service.ts
+++ src/place/place.service.ts
@@ -8,25 +8,37 @@ import { createplaceDto, updateplaceDto } from './place.dto';
 @Injectable()
 export class PlaceService {
 
-    constructor(private prisma: PrismaService){}
+    constructor(private prisma: PrismaService) { }
 
-    async create(dto: createplaceDto): Promise<Place>{
-         const { coordinates, contacts, ...rest } = dto;
+    async create(dto: createplaceDto): Promise<Place> {
+        const { coordinates, contacts, ...rest } = dto;
+        const { lat, lon } = coordinates
+        const existingCoordinate = await this.prisma.place.findFirst({
+            where: {
+                coordinates: {
+                    equals: { lat, lon }
+                },
+            },
+        });
+        if (existingCoordinate) {
+            throw new ConflictException('Coordenadas já cadastradas.');
+        }
+
         const data: Prisma.PlaceCreateInput = {
             ...dto,
             coordinates: {
-            lat: coordinates.lat,
-            lon: coordinates.lon,
+                lat: coordinates.lat,
+                lon: coordinates.lon,
             },
             ...(contacts && {
                 contacts: {
-                ...(contacts.telefone && { telefone: contacts.telefone }),
-                ...(contacts.email && { email: contacts.email }),
-                ...(contacts.site && { site: contacts.site }),
+                    ...(contacts.telefone && { telefone: contacts.telefone }),
+                    ...(contacts.email && { email: contacts.email }),
+                    ...(contacts.site && { site: contacts.site }),
                 }
             })
         };
-  return this.prisma.place.create({ data });
+        return this.prisma.place.create({ data });
     }
 
     async findAll(): Promise<Place[]> {
@@ -37,52 +49,52 @@ export class PlaceService {
         const types = type.split(','); // Divide a string em um array
         return this.prisma.place.findMany({
             where: {
-                type:{in:types}
+                type: { in: types }
             }
-        }) 
+        })
     }
 
     async findById(id: string): Promise<Place | null> {
-        const foundPlace = await this.prisma.place.findUnique({where:{id}})
+        const foundPlace = await this.prisma.place.findUnique({ where: { id } })
 
-        if(!foundPlace) {
+        if (!foundPlace) {
             throw new NotFoundException(`Local com ID ${id} não encontrado!`)
         }
 
         return foundPlace
-    }    
+    }
 
-    async update(id: string, dto:updateplaceDto): Promise<Place | null> {
-        const foundId = await this.prisma.place.findUnique({where:{id}})
+    async update(id: string, dto: updateplaceDto): Promise<Place | null> {
+        const foundId = await this.prisma.place.findUnique({ where: { id } })
 
-        if(!foundId) {
+        if (!foundId) {
             throw new NotFoundException(`Local com esse ID ${id} não encontrado!`)
         }
-    const { coordinates, contacts, ...rest } = dto;
+        const { coordinates, contacts, ...rest } = dto;
 
-  const data: Prisma.PlaceUpdateInput = {
-    ...rest,
-    ...(coordinates && {
-      coordinates: {
-        lat: coordinates.lat,
-        lon: coordinates.lon,
-      },
-    }),
-    ...(contacts && {
-      contacts: {
-        ...(contacts.telefone && { telefone: contacts.telefone }),
-        ...(contacts.email && { email: contacts.email }),
-        ...(contacts.site && { site: contacts.site }),
-      },
-    }),
-  };
-        return await this.prisma.place.update({where: {id}, data})
+        const data: Prisma.PlaceUpdateInput = {
+            ...rest,
+            ...(coordinates && {
+                coordinates: {
+                    lat: coordinates.lat,
+                    lon: coordinates.lon,
+                },
+            }),
+            ...(contacts && {
+                contacts: {
+                    ...(contacts.telefone && { telefone: contacts.telefone }),
+                    ...(contacts.email && { email: contacts.email }),
+                    ...(contacts.site && { site: contacts.site }),
+                },
+            }),
+        };
+        return await this.prisma.place.update({ where: { id }, data })
     }
 
     async remove(id: string): Promise<Place | null> {
-        try{
-            return await this.prisma.place.delete({where:{id}})
-        }catch {
+        try {
+            return await this.prisma.place.delete({ where: { id } })
+        } catch {
             throw new NotFoundException(`Local com esse ID ${id} não encontrado!`)
         }
     }
