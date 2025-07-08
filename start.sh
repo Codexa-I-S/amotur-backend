0aab969a9ee25d4e8bb5988763d804c5726e1845
Lianderson <liandersonr18@gmail.com>
2025-07-08 19:31:27 -0300

    feat: altera o start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,10 @@
 
 export DB_HOST=$(echo $DATABASE_URL | sed -E 's#.*@([^:/]+):.*#\1#')
 export DB_PORT=$(echo $DATABASE_URL | sed -E 's#.*:([0-9]+)/.*#\1#')
-export DB_USER=$(echo $DATABASE_URL | sed -E 's#postgres://([^:]+):.*#\1#')
 
 echo "Esperando o banco em $DB_HOST:$DB_PORT..."
 
-until pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USER
-do
+until nc -z $DB_HOST $DB_PORT; do
   echo "Banco não está pronto ainda. Tentando novamente em 3 segundos..."
   sleep 3
 done
